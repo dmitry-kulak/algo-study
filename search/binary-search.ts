e9acc96d9a52ba8da051a709a9a61bbb5cc12e73
dmitry-kulak <dmitry.kulak11@gmail.com>
2023-01-18 15:58:10 +0300

    additional binary search function

--- search/binary-search.ts
+++ search/binary-search.ts
@@ -1,19 +1,31 @@
-const binarySearch = (haystack: number[], needle: number): boolean => {
+export const binarySearchHas = (nums: number[], num: number): boolean => {
   let lo = 0;
-  let hi = haystack.length;
-  do {
+  let hi = nums.length;
+  while (lo < hi) {
     const m = Math.floor(lo + (hi - lo) / 2);
-    const v = haystack[m];
-    if (v === needle) {
+    if (nums[m] === num) {
       return true;
-    } else if (v > needle) {
+    } else if (nums[m] > num) {
       hi = m;
     } else {
       lo = m + 1;
     }
-  } while (lo < hi);
+  }
 
   return false;
 };
 
-export default binarySearch;
+export const binarySearch = (nums: number[], num: number): number => {
+  let lo = 0;
+  let hi = nums.length;
+  while (lo < hi) {
+    const m = Math.floor(lo + (hi - lo) / 2);
+    if (num > nums[m]) {
+      lo = m + 1;
+    } else {
+      hi = m;
+    }
+  }
+
+  return nums[lo] === num ? lo : -1;
+};
--- search/tests.ts
+++ search/tests.ts
@@ -1,5 +1,5 @@
 import linearSearch from './linear-search';
-import binarySearch from './binary-search';
+import {binarySearch, binarySearchHas} from './binary-search';
 import twoCrystalBalls from './two-crystal-balls-problem';
 
 const sortedArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -8,14 +8,23 @@ console.log('linear search');
 console.log(linearSearch(sortedArr, 3));
 console.log(!linearSearch(sortedArr, 11), '\n');
 
+console.log('binary search (has)');
+console.log(binarySearchHas(sortedArr, 1));
+console.log(binarySearchHas(sortedArr, 5));
+console.log(binarySearchHas(sortedArr, 7));
+console.log(binarySearchHas(sortedArr, 9));
+console.log(binarySearchHas(sortedArr, 10));
+console.log(!binarySearchHas(sortedArr, 11));
+console.log(!binarySearchHas(sortedArr, -1), '\n');
+
 console.log('binary search');
-console.log(binarySearch(sortedArr, 1));
-console.log(binarySearch(sortedArr, 5));
-console.log(binarySearch(sortedArr, 7));
-console.log(binarySearch(sortedArr, 9));
-console.log(binarySearch(sortedArr, 10));
-console.log(!binarySearch(sortedArr, 11));
-console.log(!binarySearch(sortedArr, -1), '\n');
+console.log(binarySearch(sortedArr, 1) === 0);
+console.log(binarySearch(sortedArr, 5) === 4);
+console.log(binarySearch(sortedArr, 7) === 6);
+console.log(binarySearch(sortedArr, 9) === 8);
+console.log(binarySearch(sortedArr, 10) === 9);
+console.log(binarySearch(sortedArr, 11) === -1);
+console.log(binarySearch(sortedArr, -1) === -1, '\n');
 
 console.log('balls');
 const breaks = [false, false, false, false, true, true, true, true, true];
