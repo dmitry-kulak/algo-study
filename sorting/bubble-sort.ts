518e1dc4df09e9eadbaaba31888dabee951791a7
dmitry-kulak <dmitry.kulak11@gmail.com>
2023-02-26 17:04:20 +0300

    fix: bubble sort swap

--- sorting/bubble-sort.ts
+++ sorting/bubble-sort.ts
@@ -6,7 +6,9 @@ export const bubbleSort: SortFn = (array) => {
   for (let i = 0; i < array.length; ++i) {
     for (let j = 0; j < array.length - i - 1; ++j) {
       if (copy[j] > copy[j + 1]) {
-        [copy[j], copy[j + 1]] = [copy[j + 1], copy[j]];
+        const tmp = copy[j];
+        copy[j] = copy[j + 1];
+        copy[j + 1] = tmp;
       }
     }
   }
